a021a0ef366285a2d6d094309c1210ff431ed903
themusicalshaman <ryanshipley27@gmail.com>
2020-11-30 13:46:37 -0700

    Looks good

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav bar
+document.querySelector("a").textContent = 'Services'
+document.querySelector("a:nth-of-type(2)").textContent = 'Product'
+document.querySelector("a:nth-of-type(3)").textContent = 'Vision'
+document.querySelector("a:nth-of-type(4)").textContent = 'Features'
+document.querySelector("a:nth-of-type(5)").textContent = 'About'
+document.querySelector("a:nth-of-type(6)").textContent = 'Contact'
+document.querySelector('a:nth-of-type(1)').style.color = 'green' 
+document.querySelector('a:nth-of-type(2)').style.color = 'green' 
+document.querySelector('a:nth-of-type(3)').style.color = 'green'
+document.querySelector('a:nth-of-type(4)').style.color = 'green'
+document.querySelector('a:nth-of-type(5)').style.color = 'green'
+document.querySelector('a:nth-of-type(6)').style.color = 'green'
+
+//button
+const button = document.querySelector('button')
+button.textContent = 'Get Started!'
+
+//middle text
+const headerTexr = document.querySelector('h1')
+headerTexr.textContent = 'DOM IS AWESOME'
+
+//Circle image
+let code_img = document.getElementById("cta-img");
+code_img.setAttribute('src', siteContent["cta"]["img-src"]); 
+//top content h4
+
+document.getElementsByTagName('h4')[0].textContent ='Features'
+document.getElementsByTagName('h4')[1].textContent ='About'
+// above middle image
+document.getElementsByTagName('p')[0].textContent ='Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+document.getElementsByTagName('p')[1].textContent ='About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+// middle image part
+let middle = document.getElementById("middle-img");
+middle.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+//below middle image
+document.getElementsByTagName('h4')[2].textContent ='Services'
+document.getElementsByTagName('h4')[3].textContent ='Product'
+document.getElementsByTagName('h4')[4].textContent ='Vision'
+//Bottom Paragraphs 
+
+document.getElementsByTagName('p')[2].textContent ='Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+document.getElementsByTagName('p')[3].textContent ='Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+document.getElementsByTagName('p')[4].textContent ='Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+//This is the contact section
+document.getElementsByTagName('h4')[5].textContent ='Contact'
+document.getElementsByTagName('p')[5].textContent = '123 456 Street Somewhere,USA'
+document.getElementsByTagName('p')[6].textContent = '1 (888) 888-8888'
+document.getElementsByTagName('p')[7].textContent = 'sales@greatIdea.io'
+//This is the footer
+document.getElementsByTagName('p')[8].textContent ="Copyright Great Idea! 2020"
+
+
